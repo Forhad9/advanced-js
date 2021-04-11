9a6749581eff2e788f17627e1de94dcf023202b8
Forhad Reza <you@.com>
2021-04-12 00:52:31 +0600

    map-filter for practise

--- map-filter.js
+++ map-filter.js
@@ -1,24 +1,24 @@
 
 const numbers = [2, 3, 4, 5, 6];
-// const output = [];
+const output = [];
 
-// for(let i = 0; i < numbers.length; i++){
-//     const element = numbers[i];
-//     const result = element * element;
-//     output.push(result);
-// }
-// function square(element){
-//     return element * element;
-// }
-// const result = numbers.map(function(element){
-//    return element * element;
+for(let i = 0; i < numbers.length; i++){
+    const element = numbers[i];
+    const result = element * element;
+    output.push(result);
+}
+function square(element){
+    return element * element;
+}
+const result = numbers.map(function(element){
+   return element * element;
    
-// })
-// const square = element => element * element;
-// const square = x => x * x;
+})
+const square = element => element * element;
+const square = x => x * x;
 
-// const result = numbers.map(x => x * x);
-// console.log(result);
+const result = numbers.map(x => x * x);
+console.log(result);
 const bigger = numbers.filter(x => x > 5);
 const bigger1 = numbers.filter(x => x > 6);
 const evenNumber = numbers.filter(x => x % 2 != 1);
