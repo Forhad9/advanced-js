db526663ffbebf410ed9f2bb3c2128a742a63c21
Forhad Reza <you@.com>
2021-04-11 12:22:10 +0600

    initial added

---
+++ true-false.js
@@ -0,0 +1,24 @@
+//Falsy:
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+
+//2.Truthly:
+//'0'
+//' '
+//[]
+//'false'
+
+
+let name =12;
+
+if(name || name == 0){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false")
+}
