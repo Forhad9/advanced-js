d40501525d25620757ff2333244c2c5e0b4428e7
Forhad Reza <you@.com>
2021-04-11 12:58:37 +0600

    null vs undefined analysis

---
+++ undefined-vs-null.js
@@ -0,0 +1,17 @@
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+function add(number1, number2){
+    console.log(number1, number2)
+
+}
+const Bio = {name: 'Ashik', age: 12, phone:32342342};
+console.log(Bio.salary);
+
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [23, 12, 31];
+console.log(ages[4]);
