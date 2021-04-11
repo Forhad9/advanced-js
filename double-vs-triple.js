46d0fbf698dd1133f605315259c420a06af15b1e
Forhad Reza <you@.com>
2021-04-11 15:23:28 +0600

    double-vs-triple for interview

---
+++ double-vs-triple.js
@@ -0,0 +1,43 @@
+// const first = 1;
+// const second = 1;
+// if(first == second){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+// const first = 2;
+// const second = "2";
+// if(first === second){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+const first = 0;
+const second = false;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+const first1 = true;
+const second1 = 1;
+if(first1 == second1){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+const first2 = true;
+const second2 = 1;
+if(first2 === second2){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
