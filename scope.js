3d03f40ea4ec7cb5f739f758c702bef50adc77e9
Forhad Reza <you@.com>
2021-04-12 18:11:13 +0600

    scope added for exprience

---
+++ scope.js
@@ -0,0 +1,19 @@
+
+let num3 = 21;
+function add(num1, num2){
+    const result = num1 + num2 + num3;
+    console.log(result);
+    console.log(num3);
+    if(result > 20){
+        const mood = "happy";
+        console.log(mood);
+    }
+    return result;
+    console.log(mood);
+    
+}
+const total = add(12, 12);
+console.log(total);
+
+// console.log(result);
+//
