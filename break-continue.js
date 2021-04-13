9c78d36188d5123cbd5fb66a8766b7cf0d08a754
Forhad Reza <you@.com>
2021-04-13 09:34:45 +0600

    break-countinue details in one

---
+++ break-continue.js
@@ -0,0 +1,9 @@
+const nums = [12, 23, 45, 65, 32, 65, 1, 32];
+for (let i = 0; i < nums.length; i++){
+    if(nums[i] > 30){
+        continue; //break dile just array ta bondho kore dibe sorto puron na hole. 1 kokhonoi asbe na array te
+        
+    }
+    console.log(nums[i]);
+}
+console.log(nums);
